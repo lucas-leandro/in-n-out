5782f666e7cfe73ea676c73077c7f28c6361601e
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-07 14:01:39 -0300

    commit inicial

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(dirname(__FILE__,2).'/src/config/config.php');
+require_once(dirname(__FILE__,2).'/src/models/User.php');
+
+foreach(User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>';
+}
