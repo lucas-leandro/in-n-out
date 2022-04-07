4c91b62fa3601131dcfcdc20ba7f9eb19cdfd3e9
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-07 17:07:28 -0300

    Tela de login e css login

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
 require_once(dirname(__FILE__,2).'/src/config/config.php');
-require_once(dirname(__FILE__,2).'/src/models/User.php');
+require_once(dirname(__FILE__,2).'/src/views/login.php');
 
-foreach(User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>';
-}
