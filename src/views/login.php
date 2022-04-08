cd0d7b0bd137804c4410abe815c9ca1a685ff8ad
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-08 15:46:07 -0300

    Criacao do menu lateral, header e footer para o template

--- src/views/login.php
+++ src/views/login.php
@@ -1,19 +1,4 @@
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/comum.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/icofont.min.css">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <title>In N' Out</title>
-    
-</head>
-<body>
     <form class="form-login" action="#" method="post">
         <div class="login-card card">
             <div class="card-header">
@@ -55,5 +40,3 @@
         </div>
     </form>
     
-</body>
-</html>
