a846c385056256a964ba89c6e61432c08cbd8605
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-09 16:38:03 -0300

    Exibiçao do dado na view

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -42,9 +42,10 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate){
 
 function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate){
     $currentDate = $initialDate;
-    $today = new DateTime();
+    $yesterday = new DateTime();
+    $yesterday ->modify('-1day');
     $columns = ['user_id' => $userId, 'work_date' => $currentDate];
-    while(isBefore($currentDate,$today)){
+    while(isBefore($currentDate,$yesterday)){
         if(!isWeekend($currentDate)){
             $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
             $columns = array_merge($columns, $template);
@@ -57,7 +58,7 @@ function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $
 }
 
 $lastMonth = strtotime('first day of last month');
-populateWorkingHours(1,date('Y-m-1'),70,20,10);
+populateWorkingHours(1,date('Y-m-d',$lastMonth),70,20,10);
 populateWorkingHours(3,date('Y-m-d',$lastMonth),20,75,5);
 populateWorkingHours(4,date('Y-m-d',$lastMonth),20,10,70);
 echo "Tudo certo!";
