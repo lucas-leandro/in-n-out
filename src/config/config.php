1d6c3747556bd45e73e4748660d43822f8198226
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-09 15:16:54 -0300

    Geracao de dados de dias trabalhados, criacao da insersao no banco de dados

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,9 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
+//constantes gerais
+define('DAILY_TIME', 60*60*8);
+
 //pastas
 define('MODEL_PATH', realpath(dirname(__FILE__).'/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__).'/../views'));
@@ -14,6 +17,7 @@ define('EXCEPTION_PATH', realpath(dirname(__FILE__).'/../exceptions'));
 require_once(realpath( dirname(__FILE__) .'/database.php') );
 require_once(realpath( dirname(__FILE__) .'/loader.php') );
 require_once(realpath( dirname(__FILE__) .'/session.php') );
+require_once(realpath( dirname(__FILE__) .'/date_utils.php') );
 require_once(realpath(MODEL_PATH.'/Model.php'));
 require_once(realpath(MODEL_PATH.'/User.php'));
 require_once(realpath(EXCEPTION_PATH.'/AppException.php'));
