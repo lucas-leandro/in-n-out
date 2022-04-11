4409ecc73867ee1bdee2184c8d7f551b73272e9a
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-11 20:02:42 -0300

    Funcao para calcular tempo gasto no horario de almoco

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -6,3 +6,7 @@ loadModel('WorkingHours');
 $wh = WorkingHours::loadFromUserAndDate(1,date('Y-m-d'));
 $workedIntervalString  = $wh->getWorkedInterval()->format('%H:%I:%S');
 print_r($workedIntervalString);
+
+echo "<br>";
+$lunchIntervalString  = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchIntervalString);
