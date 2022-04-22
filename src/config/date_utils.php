c3f83436439bfdc24b16c992a45e1b9cfdc90473
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-22 19:23:12 -0300

    funcao que retorna em string o tempo

--- src/config/date_utils.php
+++ src/config/date_utils.php
@@ -66,3 +66,12 @@ function getSecondsFromDateInterval($interval){
 function isPastWorkday($date){
     return !isWeekend($date) && isBefore($date, new DateTime());
 }
+
+function getTimeStringFromSeconds($seconds){
+    $h = intdiv($seconds, 3600);
+    $m = intdiv($seconds % 3600, 60);
+    $s = $seconds- ($h *3600) - ($m *60);
+
+    return sprintf('%02d:%02d:%02d', $h,$m,$s);
+
+}
