6382786ba884ec78fe0bffd0a26a06ad485503c4
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-22 21:57:38 -0300

    Finalizaçao da view e funcao para calculo saldo de horas trabalhadas

--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -5,6 +5,7 @@
         'Acompanhe seu saldo de horas',
         'icofont-ui-calendar'
     );
+    
 ?>
 
     <div>
@@ -20,12 +21,12 @@
             <tbody>
                 <?php  foreach($report as $registry){?>
                     <tr>
-                        <td><?= $registry->work_date?></td>
+                        <td><?= formatDateWithLocale($registry->work_date, '%A, de $B de %Y')?></td>
                         <td><?= $registry->time1?></td>
                         <td><?= $registry->time2?></td>
                         <td><?= $registry->time3?></td>
                         <td><?= $registry->time4?></td>
-                        <td><?= "Saldo"?></td>
+                        <td><?=$registry->getBalance()?></td>
                     </tr>
                 <?}?>
                     <tr class="bg-primary text-white">
