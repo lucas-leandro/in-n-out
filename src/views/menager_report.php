5569957cadcfb96d5c8d32d331d24e3555e3ee27
Lucas Leandro <lucasleandro159@icloud.com>
2022-04-26 17:10:41 -0300

    Criacao do controller e da view manager report e direcionamento ao clicar em menager report para o local certo

---
+++ src/views/menager_report.php
@@ -0,0 +1,3 @@
+<main class="content">
+    <H1>Ok</H1>
+</main>
--- src/views/template/left.php
+++ src/views/template/left.php
@@ -16,7 +16,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="day_recods.php">
+                <a href="menager_report.php">
                     <i class="icofont-chart-histogram mr-2">
                         Relatorio Gerencial
                     </i>
