4d9c61684a2b4082d244e3cc1082a0ca6cf1e5ee
Lucas Leandro <lucasleandro159@icloud.com>
2022-05-02 20:11:05 -0300

    Criacao do controller e da view do save_user, que vai fazer update e create de usuarios

---
+++ src/views/save_user.php
@@ -0,0 +1,11 @@
+<main class="content">
+    <?php
+        renderTitle(
+            'Cadastro de Usuario',
+            'Crie e atualize o usuario',
+            'icofont-user'
+        );
+        include(TEMPLATE_PATH."/message.php");
+    ?>
+
+</main>
