f8850d6b1b89e8405dc30242beb8380861c0aa44
Lucas Leandro <lucasleandro159@icloud.com>
2022-05-02 20:03:39 -0300

    alteracao da formatacao da data de entrada e saida no controller

--- src/controllers/users.php
+++ src/controllers/users.php
@@ -2,7 +2,12 @@
 session_start();
 requireValidSession(); 
 
-
 $users = User::get();
+foreach($users as $user){
+    $user->start_date = (new DateTime($user->start_date))->format('d/m/Y');
+    if($user->end_date){
+        $user->end_date = (new DateTime($user->start_date))->format('d/m/Y');
+    }
+}
 
 loadTemplateView('users', ['users' => $users]);
